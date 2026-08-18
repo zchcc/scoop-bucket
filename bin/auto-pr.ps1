f01cdfd392540b6829a77dfccc869531ca3b6105
zchcc <zchcc1991@gmail.com>
2026-08-18 10:51:45 +0800

    Change upstream parameter to 'zchcc/scoop-bucket:master'

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "zchcc/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
